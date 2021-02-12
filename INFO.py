e9785768ef60c44ce4429b5f08ee4481b9347715
syedfardeen697 <69422091+syedfardeen697@users.noreply.github.com>
2021-02-12 23:33:36 +0530

    Add files via upload

---
+++ INFO.py
@@ -0,0 +1,7 @@
+header="TOOL BY FARDEEN";
+align = header.center(60);
+print(align);
+name=input("ENTER YOUR NAME: ");
+age=input(name+");" + " ENTER YOUR AGE: ");
+skills=input(name+");"+ " ENTER YOUR SKILLS: ");
+print("INFO OBTAINED \n" + "NAME:>> " + name + "\n" + "AGE:>> " + age + "\n" + "SKILLS:>> " + skills);
